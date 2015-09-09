1e25e99cc37f15a8db2139dac925665e9bcc006d
clevermorad <bmorad52@yahoo.fr>
2015-09-09 14:37:31 +0200

    debut Entreprise

--- tests/tp2/EnterpriseTest.php
+++ tests/tp2/EnterpriseTest.php
@@ -7,5 +7,32 @@ use tp2\Person;
 
 class EnterpriseTest extends \PHPUnit_Framework_TestCase
 {
+	protected $person;
+	protected $entreprise;
+
+	public function setUp()
+	{
+		$this->person = new Person('toto');
+		$this->entreprise = new Enterprise();
+	}
+
+	public function testadd()
+    {
+       
+    	$this->entreprise->add($person);
+    }
+
+    public function testremove()
+    {
+        // TO IMPLEMENT
+    }
+
+    /**
+     * @return boolean
+     */
+    public function testemploy()
+    {
+        // TO IMPLEMENT
+    }
 
 }
--- tests/tp2/PersonTest.php
+++ tests/tp2/PersonTest.php
@@ -6,6 +6,16 @@ use tp2\Person;
 
 class PersonTest extends \PHPUnit_Framework_TestCase
 {
-	$person = new Person('toto');
-	$this->assertEquals('toto',$this->getName());
+	protected $person;
+
+	public function setUp()
+	{
+		$this->person = new Person('toto');
+	}
+
+	public function testGetName()
+	{
+		
+	    $this->assertEquals('toto',$this->person->getName());
+	}
 }
